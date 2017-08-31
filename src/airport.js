592680dd2be6dd74cdcb90ef0f717d194a3a8366
Abi Travers <atravers4007@gmail.com>
2017-08-31 13:17:30 +0100

    1st attempt at airport class

--- src/airport.js
+++ src/airport.js
@@ -1,8 +1,17 @@
 //supreme function / variable
- 
-var Airport = function(){};
 
-Airport.prototype.planes = {plane1 :1}
+var Airport = function(planes){
+  this.planesz = planes;
+};
 
+Airport.prototype.planesarray = function(){
+  planes = new Array(this.planesz);
+};
 
-Airport.prototype.landed =
+//TypeError: air is not a function
+air = new Airport
+air("planes")
+
+air = new airport
+air("planes")
+//TypeError: air is not a function
