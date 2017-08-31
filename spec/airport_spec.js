fb9eeabcd3299465a93df5f2e167a4ab16365813
Abi Travers <atravers4007@gmail.com>
2017-08-31 14:08:20 +0100

    aiport spec for having empty plane array

--- spec/airport_spec.js
+++ spec/airport_spec.js
@@ -7,18 +7,7 @@ describe("Airport", function() {
     plane1 = new Plane();
   });
 
-
-it("a plane can land in the airport", function(){
-  airport.landing(plane1);
-  expect(airport.planes).to include(plane1);
-  //landing
+  it("it has no planes as a default", function(){
+    expect(airport.planes()).toEqual([]);
+  });
 });
-
-  //
-  // it("should be able to play a Song", function() {
-  //   player.play(song);
-  //   expect(player.currentlyPlayingSong).toEqual(song);
-  //
-  //   //demonstrates use of custom matcher
-  //   expect(player).toBePlaying(song);
-  // });
