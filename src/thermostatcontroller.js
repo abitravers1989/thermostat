abb426cf98a4eef44fef72bd45f31366c9d68123
Abi Travers <atravers4007@gmail.com>
2017-09-02 07:25:04 +0100

    Further jquery added

--- src/thermostatcontroller.js
+++ src/thermostatcontroller.js
@@ -1,7 +1,13 @@
 $( document ).ready(function(){
 
+  $('#temperature').text ('HOW hot? ARE YOU CRAZY FOOL??!!');
+
   $( "input" ).click(function( event ) {
     alert("Welcome to the world of heat");
+  });
+
+  $( '#up').click(function(){
+    alert("turning it up")
   });
 
 });
